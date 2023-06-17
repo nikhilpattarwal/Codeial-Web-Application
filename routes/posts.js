fd5a29dca10eaad1f70391f3f0dd4d0b6497a30f
Nikhil <nikhilpatterwal123@gmail.com>
2023-06-17 17:59:12 +0530

    some changes to footer.scss

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router(); // It is creating an instance of an Express router.
 
+const passport = require('passport');
+
 const postController = require('../controllers/posts_controller');
 
-router.post('/create', postController.create);
+router.post('/create', passport.checkAuthentication, postController.create);
 
 module.exports = router; 
 
